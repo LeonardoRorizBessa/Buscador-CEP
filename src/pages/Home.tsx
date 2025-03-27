8521a90122219513e6d2335af1054c3a492ec5a0
LeonardoRorizBessa <leonardo.rorizbessa@gmail.com>
2025-03-27 14:52:40 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return ( 
+    <>
+      <section className="bg-preto h-screen flex justify-center items-center">
+        <div className="bg-cinza rounded-lg p-12">
+          
+        </div>
+      </section>
+    </>
+   );
+}
+ 
+export default Home;
